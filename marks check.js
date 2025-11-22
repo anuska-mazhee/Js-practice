ac53a86bd90d4b8c40d7cf2b2040498178dbd167
anuska-mazhee <anuskamazhee@gmail.com>
2025-11-22 22:40:28 +0545

    marksheet check

---
+++ marks check.js
@@ -0,0 +1,16 @@
+    let num = parseInt(prompt("enter marks: "));
+if (num>= 90){
+    console.log("Result : A");
+}
+else if (num>=80){
+    console.log("Result : B");
+}
+else if (num>=70){
+    console.log("Result : C");
+}
+else if (num>=60){
+    console.log("Result : D");
+}
+else{
+    console.log("Result : NG");
+}
