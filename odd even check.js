ff3e9bef77980a7d444a6aabec3949d67f14b929
anuska-mazhee <anuskamazhee@gmail.com>
2025-11-25 12:42:06 +0545

    new check

---
+++ odd even check.js
@@ -0,0 +1,10 @@
+let num = prompt("enter num : ");
+if (num == ""){
+    console.log("you did not enter any num.");
+}
+else if (num %2 == 0){
+    console.log("even num");
+}
+else if (num%2 != 0){
+    console.log("odd num");
+}
