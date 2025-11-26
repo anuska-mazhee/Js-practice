ed623d05c8858fd00aa0c9e9633be4dea540e457
anuska-mazhee <anuskamazhee@gmail.com>
2025-11-26 12:39:22 +0545

    new input table

---
+++ input table.js
@@ -0,0 +1,5 @@
+let n = prompt("Enter the num you want to multiply : ");
+let t = prompt("Enter how many times you want to multiply : ");
+for ( let i = 1; i<=t;i++){
+    console.log(n," x ",i,"=",n*i);
+}
