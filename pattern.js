a0089b59089664cb0f1c90daac9935ac5ce052c9
anuska-mazhee <anuskamazhee@gmail.com>
2025-12-13 17:34:40 +0545

    pattern

---
+++ pattern.js
@@ -0,0 +1,11 @@
+//WAP to make a pattern of anything.
+for(i=1;i<=5;i++)
+{
+    let x = "";
+    for(j=0;j<i;j++)
+    {
+        x += "anu ";
+    }
+    console.log(x);
+    console.log("\n");
+}
