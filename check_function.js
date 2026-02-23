79d4f39a64ec57cdd73c33eac8b94e84780b933f
anuska-mazhee <anuskamazhee@gmail.com>
2026-02-23 16:31:17 +0545

    odd even check using function

---
+++ check_function.js
@@ -0,0 +1,13 @@
+function check(a)
+{
+    if (a % 2 == 0)
+    {
+        console.log(`${a} is even`);
+    }
+    else
+    {
+        console.log(`${a} is odd.`);
+    }
+}
+let result = check(5);
+console.log(result);
